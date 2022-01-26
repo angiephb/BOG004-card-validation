97c77f4573339a7cafbff87cb839a2413731fb5a
angiephb <angie.pio30@gmail.com>
2022-01-26 15:00:42 -0500

    cambios validator

--- src/validator.js
+++ src/validator.js
@@ -3,20 +3,27 @@ const validator = {
 /* Aquí tomamos el metodo "isValid" para ejecutar dentro cada funcion del algoritmo de luhn */
 
   isValid:function(creditCardNumber){
-   creditCardNumber.reverse();
+  creditCardNumber.reverse();
 
-    console.log("valortarjeta",creditCardNumber);
+    console.log("valortarjeta", creditCardNumber);
+
+/*Utilizamos el metodo .map para recorrer el array y nos devolviera cada elemento convertido en un entero*/
+
+  let Enteros = creditCardNumber.map(function (num){
+    return  parseInt(num, 10);    
+  })
+  console.log("enteros", Enteros);
 
 /*numeros impares y pares: filtramos cada el elemento del array con .filter por numero y posicion
-  para obtener los numeros pares e impares y guardarlos en un array diferente*/
+para obtener los numeros pares e impares y guardarlos en un array diferente*/
 
-    let impares =creditCardNumber.filter (function (num,position){
-        return (num,position % 2 != 0);             
+    let impares = Enteros.filter (function (num,position){
+      return (num,position % 2 != 1);   
     })
     console.log("impares", impares);
 
-    let pares =creditCardNumber.filter (function (num,position){
-    return (num,position % 2 == 0);             
+    let pares = Enteros.filter (function (num,position){
+    return (num,position % 2 == 1);             
     })
     console.log("pares", pares);
 
@@ -24,14 +31,39 @@ const validator = {
 en las pocisiones pares*/
 
     let dobles = pares.map(function (num){
-  let multi = num*2
-  if (multi > 9) {
-    multi = multi - 9
-  } 
-  return multi;
+      let multi = num*2
+      if (multi > 9) {
+        multi = multi - 9
+      } 
+      return multi;
     })
-    console.log("multi", dobles);
+    console.log("dobles", dobles);
+
+    let digitos = dobles.concat(impares);   
+    console.log("digitos", digitos);
+    
+    let resultados =  digitos.reduce(function(valorAnterior, valorActual){
+       return valorAnterior + valorActual;
+    })
+
+    console.log("resultados", resultados);
+    /*
+
+    let sumap = dobles.reduce(function(valorAnterior, valorActual){
+      return valorAnterior + valorActual;
+    });
+    console.log("sumai", sumap);
+    
+    let sumai = impares.reduce(function(valorAnterior, valorActual){
+      return valorAnterior + valorActual;
+    });
+    console.log("sumap", sumai);
+    
+    let resultado = (sumap + sumai) % 10 === 0
+      return resultado;   */  
   }
+  
+    
 
     
   
