77d3a808d238431ccd7dd98a93656323db3bc674
LilianaRuizC <angoraazul@hotmail.com>
2022-01-26 14:04:12 -0600

    nada

--- src/validator.js
+++ src/validator.js
@@ -4,19 +4,21 @@ const validator = {
 
   isValid:function(creditCardNumber){
    creditCardNumber.reverse();
-
-    console.log("valortarjeta",creditCardNumber);
+   
+   console.log("valortarjeta",creditCardNumber);
 
 /*numeros impares y pares: filtramos cada el elemento del array con .filter por numero y posicion
   para obtener los numeros pares e impares y guardarlos en un array diferente*/
-
+      console.log("digito",digitoA)
     let impares =creditCardNumber.filter (function (num,position){
-        return (num,position % 2 != 0);             
+      
+        return (num,position % 2 !=1)            
     })
     console.log("impares", impares);
+  
 
     let pares =creditCardNumber.filter (function (num,position){
-    return (num,position % 2 == 0);             
+    return (num,position % 2 == 1);             
     })
     console.log("pares", pares);
 
@@ -31,6 +33,7 @@ en las pocisiones pares*/
   return multi;
     })
     console.log("multi", dobles);
+  
   }
 
     
