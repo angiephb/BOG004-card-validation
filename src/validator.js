0d260e4ad8b7a37c15c7d83bd118231595c117d7
angiephb <angie.pio30@gmail.com>
2022-01-28 15:00:34 -0500

    cambios test y validator

--- src/validator.js
+++ src/validator.js
@@ -3,21 +3,17 @@ const validator = {
 /* Aquí tomamos el metodo "isValid" para ejecutar dentro cada funcion del algoritmo de luhn */
 
   isValid:function(creditCardNumber){
+
+/*convertimos creditCardNumber en string */
     creditCardNumber = creditCardNumber.split('').reverse();
 
+/*Utilizamos el metodo .map para recorrer el array y nos devolviera cada
+elemento convertido en un entero*/
 
-<<<<<<< HEAD
-   /* console.log("valortarjeta", creditCardNumber);*/
-=======
-    /*console.log("valortarjeta", creditCardNumber);*/
->>>>>>> 6e2813f1817f689befce6d42d047394741dfb642
-
-/*Utilizamos el metodo .map para recorrer el array y nos devolviera cada elemento convertido en un entero*/
-
-  let Enteros = creditCardNumber.map (function (num){
-    return  parseInt(num, 10);    
-  })
-  /*console.log("enteros", Enteros);*/
+    let Enteros = creditCardNumber.map (function (num){
+      return  parseInt(num, 10);    
+    })
+    /*console.log("enteros", Enteros);*/
 
 /*numeros impares y pares: filtramos cada el elemento del array con .filter por numero y posicion
 para obtener los numeros pares e impares y guardarlos en un array diferente*/
@@ -25,22 +21,13 @@ para obtener los numeros pares e impares y guardarlos en un array diferente*/
     let impares = Enteros.filter (function (num,position){
       return (num,position % 2 != 1);   
     })
-<<<<<<< HEAD
-   /* console.log("impares", impares);*/
-=======
     /*console.log("impares", impares);*/
->>>>>>> 6e2813f1817f689befce6d42d047394741dfb642
-  
 
     let pares = Enteros.filter (function (num,position){
-    return (num,position % 2 == 1);             
+      return (num,position % 2 == 1);             
     })
-<<<<<<< HEAD
-   /* console.log("pares", pares);*/
-=======
-    /*console.log("pares", pares);*/
->>>>>>> 6e2813f1817f689befce6d42d047394741dfb642
-
+    /* console.log("pares", pares);*/
+    
 /*Aquí utilizamos el metodo map para obtener un array con los resultados de las operaciones
 en las pocisiones pares, ejecutamos multiplicacion y suma de los digitos mayores que 9*/
 
@@ -55,12 +42,6 @@ en las pocisiones pares, ejecutamos multiplicacion y suma de los digitos mayores
 
     let digitos = dobles.concat(impares);   
     /*console.log("digitos", digitos);*/
-<<<<<<< HEAD
-=======
-
-/* Aqui utilizamos el metodo .reduce, para que nos acumulara o sumara todos los digitos
-y nos diera un numero de 2 cifras */
->>>>>>> 6e2813f1817f689befce6d42d047394741dfb642
 
     let suma =  digitos.reduce((acc, valor) => acc + valor, 0);
     let resultados = suma % 10 === 0;
@@ -75,7 +56,7 @@ y lo trajera enmascarando las primeras 12 cifras*/
     return creditCardNumber.map( (num, position) => {
       return (position < creditCardNumber.length - 4 ) ? '#': num;
     }).join('');
-},
+    },
 };
 
 export default validator;
