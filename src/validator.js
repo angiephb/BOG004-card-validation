0ffb78ec3def16c0df1f27749dddddeef72eeba9
LilianaRuizC <angoraazul@hotmail.com>
2022-01-28 11:39:47 -0600

    ingreso nuestro readmeTC

--- src/validator.js
+++ src/validator.js
@@ -5,14 +5,14 @@ const validator = {
   isValid:function(creditCardNumber){
   creditCardNumber.reverse();
 
-    console.log("valortarjeta", creditCardNumber);
+   /* console.log("valortarjeta", creditCardNumber);*/
 
 /*Utilizamos el metodo .map para recorrer el array y nos devolviera cada elemento convertido en un entero*/
 
   let Enteros = creditCardNumber.map(function (num){
     return  parseInt(num, 10);    
   })
-  console.log("enteros", Enteros);
+  /*console.log("enteros", Enteros);*/
 
 /*numeros impares y pares: filtramos cada el elemento del array con .filter por numero y posicion
 para obtener los numeros pares e impares y guardarlos en un array diferente*/
@@ -20,13 +20,13 @@ para obtener los numeros pares e impares y guardarlos en un array diferente*/
     let impares = Enteros.filter (function (num,position){
       return (num,position % 2 != 1);   
     })
-    console.log("impares", impares);
+   /* console.log("impares", impares);*/
   
 
     let pares = Enteros.filter (function (num,position){
     return (num,position % 2 == 1);             
     })
-    console.log("pares", pares);
+   /* console.log("pares", pares);*/
 
 /*Aquí utilizamos el metodo map para obtener un array con los resultados de las operaciones
 en las pocisiones pares*/
@@ -38,10 +38,10 @@ en las pocisiones pares*/
       } 
       return multi;
     })
-    console.log("dobles", dobles);
+    /*console.log("dobles", dobles);*/
 
     let digitos = dobles.concat(impares);   
-    console.log("digitos", digitos);
+    /*console.log("digitos", digitos);*/
 
     let suma =  digitos.reduce((acc, valor) => acc + valor, 0);
     let resultados = suma % 10 === 0;
